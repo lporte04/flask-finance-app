fa3ecab8c8ba4ebee8c944c65f7fd8afed3ded98
Liam Porter <lappqp@gmail.com>
2025-04-29 14:20:36 -0400

    fix conflict with main?

--- app/models.py
+++ app/models.py
@@ -2,24 +2,19 @@ from datetime import date
 from app import db
 from flask_login import UserMixin
 
-
-class User(db.Model,
-           UserMixin):  # UserMixin is a class from Flask-Login that provides the methods needed for user auth.
+class User(db.Model, UserMixin): # UserMixin is a class from Flask-Login that provides the methods needed for user auth.
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
-    accounts = db.relationship('Account', backref='owner',
-                               lazy=True)  # 1-to-many relationship with Account (its here but I dont really think we'll have users with multiple accounts). Backref allows us to access the owner of an account from the account object.
-
+    accounts = db.relationship('Account', backref='owner', lazy=True) # 1-to-many relationship with Account (its here but I dont really think we'll have users with multiple accounts). Backref allows us to access the owner of an account from the account object.
 
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     current_balance = db.Column(db.Float, nullable=False)
     min_balance_goal = db.Column(db.Float, nullable=False)
-    weekly_spending_limit = db.Column(db.Float,
-                                      nullable=False)  # to store how much money the user wants to spend this week. tbd if this is used.
+    weekly_spending_limit = db.Column(db.Float, nullable=False) # to store how much money the user wants to spend this week. tbd if this is used.
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     hourly_wage = db.Column(db.Float, nullable=True)
     hours_per_week = db.Column(db.Float, nullable=True)
@@ -30,24 +25,21 @@ class Account(db.Model):
     investments = db.relationship('Investment', backref='account', lazy=True)
     assets = db.relationship('Asset', backref='account', lazy=True)
 
-
-class RecurringExpense(db.Model):  # This is a class to store recurring expenses like rent, subscriptions, etc.
+class RecurringExpense(db.Model): # This is a class to store recurring expenses like rent, subscriptions, etc.
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     frequency = db.Column(db.String(20), nullable=False)  # daily, weekly, monthly. Should probably be using an enum.
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
-
-class Spending(db.Model):  # Tracks purchases made by the user that are not scheduled recurring expenses.
+class Spending(db.Model): # Tracks purchases made by the user that are not scheduled recurring expenses.
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.Date, default=date.today)
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
 
-
-class SavingsGoal(db.Model):  # Let the user set a savings goal for a specific item.
+class SavingsGoal(db.Model): # Let the user set a savings goal for a specific item.
     id = db.Column(db.Integer, primary_key=True)
     item = db.Column(db.String(100), nullable=False)
     cost = db.Column(db.Float, nullable=False)
@@ -56,17 +48,16 @@ class SavingsGoal(db.Model):  # Let the user set a savings goal for a specific i
     # This is a relationship to the SavingsDeposit class. It allows us to access all deposits made towards this goal.
     # cascade="all, delete-orphan" means that if a SavingsGoal is deleted, all its associated SavingsDeposits will also be deleted.
     deposits = db.relationship('SavingsDeposit', backref='goal', lazy=True, cascade="all, delete-orphan")
-
+    
     # This is a property that calculates the current amount saved towards the goal by summing up all deposits.
     @property
     def current_amount(self):
         return sum(deposit.amount for deposit in self.deposits)
-
+    
     # This is a property that checks if the goal is funded. It returns True if the current amount is greater than or equal to the cost of the item.
     @property
     def is_funded(self):
         return self.current_amount >= self.cost
-
 
 class SavingsDeposit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -74,13 +65,11 @@ class SavingsDeposit(db.Model):
     date = db.Column(db.Date, default=date.today)
     savings_goal_id = db.Column(db.Integer, db.ForeignKey('savings_goal.id'), nullable=False)
 
-
-class Investment(db.Model):  # Minimalistic investment tracking. Expand later.
+class Investment(db.Model): # Minimalistic investment tracking. Expand later.
     id = db.Column(db.Integer, primary_key=True)
     stock_name = db.Column(db.String(50), nullable=False)
-    amount = db.Column(db.Float, nullable=False)  # Amount invested in the stock.
+    amount = db.Column(db.Float, nullable=False) # Amount invested in the stock.
     account_id = db.Column(db.Integer, db.ForeignKey('account.id'), nullable=False)
-
 
 class Asset(db.Model):
     id = db.Column(db.Integer, primary_key=True)
